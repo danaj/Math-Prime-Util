3419e58fea7b6bf5cdc92aae79876587721f6f57
Dana Jacobsen <dana@acm.org>
2012-12-20 01:02:48 -0800

    Update versions and dates for release

--- lib/Math/Prime/Util.pm
+++ lib/Math/Prime/Util.pm
@@ -5,7 +5,7 @@ use Carp qw/croak confess carp/;
 
 BEGIN {
   $Math::Prime::Util::AUTHORITY = 'cpan:DANAJ';
-  $Math::Prime::Util::VERSION = '0.16';
+  $Math::Prime::Util::VERSION = '0.17';
 }
 
 # parent is cleaner, and in the Perl 5.10.1 / 5.12.0 core, but not earlier.
@@ -1750,7 +1750,7 @@ Math::Prime::Util - Utilities related to prime numbers, including fast sieves an
 
 =head1 VERSION
 
-Version 0.16
+Version 0.17
 
 
 =head1 SYNOPSIS
