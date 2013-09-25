9f816a89c7cb258b894db979dfa81b942ea270a9
Dana Jacobsen <dana@acm.org>
2013-09-24 19:00:33 -0700

    Add stopword for newest spellchecker

--- lib/Math/Prime/Util.pm
+++ lib/Math/Prime/Util.pm
@@ -2487,7 +2487,7 @@ __END__
 
 =encoding utf8
 
-=for stopwords forprimes Möbius Deléglise totient moebius mertens znorder irand primesieve uniqued k-tuples von SoE pari yafu fonction qui compte le nombre nombres voor PhD superset sqrt(N) gcd(A^M k-th
+=for stopwords forprimes Möbius Deléglise totient moebius mertens znorder irand primesieve uniqued k-tuples von SoE pari yafu fonction qui compte le nombre nombres voor PhD superset sqrt(N) gcd(A^M k-th (10001st
 
 
 =head1 NAME
