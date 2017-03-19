dbb675e5e7969d482fa50a074176cc26f746877e
Dana Jacobsen <dana@acm.org>
2017-03-19 22:39:00 +0100

    Speed up is_pillai for composites (early 0 detection)

--- util.c
+++ util.c
@@ -2330,7 +2330,7 @@ int is_semiprime(UV n) {
 UV pillai_v(UV n) {
   UV v, fac = 5040 % n;
   if (n == 0) return 0;
-  for (v = 8; v < n-1; v++) {
+  for (v = 8; v < n-1 && fac != 0; v++) {
     fac = (n < HALF_WORD) ? (fac*v) % n : mulmod(fac,v,n);
     if (fac == n-1 && (n % v) != 1)
       return v;
