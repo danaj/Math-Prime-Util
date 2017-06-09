a0497f54cd1d5a0ed59f41ddd6adf6270749d80b
Dana Jacobsen <dana@acm.org>
2017-06-09 10:33:07 -0700

    Hensel lifting in Montgomery inverse

--- montmath.h
+++ montmath.h
@@ -87,30 +87,16 @@ static INLINE UV mont_powmod64(uint64_t a, uint64_t k, uint64_t one, uint64_t n,
   return t;
 }
 /* Returns -a^-1 mod 2^64.  From B. Arazi "On Primality Testing Using Purely
- * Divisionless Operations", Computer Journal (1994) 37 (3): 219-222, Proc 5 */
+ * Divisionless Operations", Computer Journal (1994) 37 (3): 219-222, Proc 5.
+ * Hensel lifting idea from Robert Gerbicz. */
 static INLINE uint64_t modular_inverse64(const uint64_t a)
 {
-  uint64_t J, S = 1;
-  int idx;
-  /* Basic algorithm:
-   *    for (i = 0; i < 64; i++) {
-   *      if (S & 1)  {  J |= (1ULL << i);  S += a;  }
-   *      S >>= 1;
-   *    }
-   * What follows is 8 bits at a time, unrolled by hand. */
   static const char mask[128] = {255,85,51,73,199,93,59,17,15,229,195,89,215,237,203,33,31,117,83,105,231,125,91,49,47,5,227,121,247,13,235,65,63,149,115,137,7,157,123,81,79,37,3,153,23,45,11,97,95,181,147,169,39,189,155,113,111,69,35,185,55,77,43,129,127,213,179,201,71,221,187,145,143,101,67,217,87,109,75,161,159,245,211,233,103,253,219,177,175,133,99,249,119,141,107,193,191,21,243,9,135,29,251,209,207,165,131,25,151,173,139,225,223,53,19,41,167,61,27,241,239,197,163,57,183,205,171,1};
-
-  const char amask = mask[(a >> 1) & 127];
-  uint32_t T;
-  idx = (amask*(S&255)) & 255;  J = idx;                  S = (S+a*idx) >> 8;
-  idx = (amask*(S&255)) & 255;  J |= (uint64_t)idx << 8;  S = (S+a*idx) >> 8;
-  idx = (amask*(S&255)) & 255;  J |= (uint64_t)idx <<16;  S = (S+a*idx) >> 8;
-  idx = (amask*(S&255)) & 255;  J |= (uint64_t)idx <<24;  T = (S+a*idx) >> 8;
-  idx = (amask*(T&255)) & 255;  J |= (uint64_t)idx <<32;  T = (T+a*idx) >> 8;
-  idx = (amask*(T&255)) & 255;  J |= (uint64_t)idx <<40;  T = (T+a*idx) >> 8;
-  idx = (amask*(T&255)) & 255;  J |= (uint64_t)idx <<48;  T = (T+a*idx) >> 8;
-  idx = (amask*(T&255)) & 255;  J |= (uint64_t)idx <<56;
-  return J;
+  uint64_t ret = mask[(a >> 1) & 127];
+  ret *= 2 + a * ret;
+  ret *= 2 + a * ret;
+  ret *= 2 + a * ret;
+  return ret;
 }
 static INLINE uint64_t compute_modn64(const uint64_t n)
 {
