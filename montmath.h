b06f754696304b47ae461799a4391e5f4d16d6a9
Dana Jacobsen <dana@acm.org>
2017-10-10 23:50:46 -0700

    Remove second perfect square pre-test on x86-64

--- montmath.h
+++ montmath.h
@@ -11,137 +11,89 @@
 #endif
 
 #if USE_MONTMATH
-/******************************************************************************/
-/*                   This is the interface we'll use                          */
-/******************************************************************************/
-#define mont_inverse(n)           modular_inverse64(n)
-#define mont_get1(n)              compute_modn64(n)
+
+#define mont_get1(n)              _u64div(1,n)
 /* Must have npi = mont_inverse(n), mont1 = mont_get1(n) */
 #define mont_get2(n)              addmod(mont1,mont1,n)
 #define mont_geta(a,n)            mulmod(a,mont1,n)
-#define mont_mulmod(a,b,n)        mont_prod64(a,b,n,npi)
-#define mont_sqrmod(a,n)          mont_prod64(a,a,n,npi)
-#define mont_powmod(a,k,n)        mont_powmod64(a,k,mont1,n,npi)
+#define mont_mulmod(a,b,n)        _mulredc(a,b,n,npi)
+#define mont_sqrmod(a,n)          _mulredc(a,a,n,npi)
+#define mont_powmod(a,k,n)        _powredc(a,k,mont1,n,npi)
 #define mont_recover(a,n)         mont_mulmod(a,1,n)
-/******************************************************************************/
-/******************************************************************************/
 
-
-
-/******************************************************************************
-  Montgomery math and efficient M-R from
-  Wojciech Izykowski.  See:  https://github.com/wizykowski/miller-rabin
-
-Copyright (c) 2013-2014, Wojciech Izykowski
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * The name of the author may not be used to endorse or promote products
-      derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-******************************************************************************/
-
-#if defined(__GNUC__) && (__GNUC__ >= 3)
- #define MPU_UNLIKELY(x) __builtin_expect(!!(x), 0)
+static INLINE uint64_t mont_inverse(const uint64_t n) {
+#if 1
+  uint64_t ret = (((n + 2) & 4) << 1) + n;
+  ret *= 2 - n * ret;
+  ret *= 2 - n * ret;
+  ret *= 2 - n * ret;
+  ret *= 2 - n * ret;
+  return (uint64_t)0 - ret;
 #else
- #define MPU_UNLIKELY(x) (x)
+  static const char mask[128] = {255,85,51,73,199,93,59,17,15,229,195,89,215,237,203,33,31,117,83,105,231,125,91,49,47,5,227,121,247,13,235,65,63,149,115,137,7,157,123,81,79,37,3,153,23,45,11,97,95,181,147,169,39,189,155,113,111,69,35,185,55,77,43,129,127,213,179,201,71,221,187,145,143,101,67,217,87,109,75,161,159,245,211,233,103,253,219,177,175,133,99,249,119,141,107,193,191,21,243,9,135,29,251,209,207,165,131,25,151,173,139,225,223,53,19,41,167,61,27,241,239,197,163,57,183,205,171,1};
+  uint64_t ret = mask[(n >> 1) & 127];
+  ret *= 2 + n * ret;
+  ret *= 2 + n * ret;
+  ret *= 2 + n * ret;
+  return ret;
 #endif
-
-static INLINE uint64_t mont_prod64(uint64_t a, uint64_t b, uint64_t n, uint64_t npi)
-{
-  uint64_t t_hi, t_lo, m, mn_hi, mn_lo, u;
-  /* t_hi * 2^64 + t_lo = a*b */
-  asm("mulq %3" : "=a"(t_lo), "=d"(t_hi) : "a"(a), "rm"(b));
-  if (MPU_UNLIKELY(t_lo == 0)) return t_hi;  /* Allows carry=1 below */
-  m = t_lo * npi;
-  /* mn_hi * 2^64 + mn_lo = m*n */
-  asm("mulq %3" : "=a"(mn_lo), "=d"(mn_hi) : "a"(m), "rm"(n));
-  u = t_hi + mn_hi + 1;
-  return (u < t_hi || u >= n)  ?  u-n  :  u;
 }
-#define mont_square64(a, n, npi)  mont_prod64(a, a, n, npi)
-static INLINE UV mont_powmod64(uint64_t a, uint64_t k, uint64_t one, uint64_t n, uint64_t npi)
-{
+
+static INLINE uint64_t _mulredc(uint64_t a, uint64_t b, uint64_t n, uint64_t npi) {
+  /* asm from Ben Buhrow */
+  if (n & 0x8000000000000000ULL) {
+    asm("mulq %1 \n\t"
+        "movq %%rax, %%r10 \n\t"
+        "movq %%rdx, %%r11 \n\t"
+        "movq $0, %%r12 \n\t"
+        "mulq %2 \n\t"
+        "mulq %3 \n\t"
+        "addq %%r10, %%rax \n\t"
+        "adcq %%r11, %%rdx \n\t"
+        "cmovae %3, %%r12 \n\t"
+        "xorq %%rax, %%rax \n\t"
+        "subq %3, %%rdx \n\t"
+        "cmovc %%r12, %%rax \n\t"
+        "addq %%rdx, %%rax \n\t"
+        : "+&a"(a)
+        : "r"(b), "r"(npi), "r"(n)
+        : "rax", "rdx", "r10", "r11", "r12", "cc");
+  } else {
+    asm("mulq %2 \n\t"
+        "movq %%rax, %%r10 \n\t"
+        "movq %%rdx, %%r11 \n\t"
+        "mulq %3 \n\t"
+        "mulq %4 \n\t"
+        "addq %%r10, %%rax \n\t"
+        "adcq %%r11, %%rdx \n\t"
+        "xorq %%rax, %%rax \n\t"
+        "subq %4, %%rdx \n\t"
+        "cmovc %4, %%rax \n\t"
+        "addq %%rdx, %%rax \n\t"
+        : "=a"(a)
+        : "0"(a), "r"(b), "r"(npi), "r"(n)
+        : "rdx", "r10", "r11", "cc");
+  }
+  return a;
+}
+
+static INLINE UV _powredc(uint64_t a, uint64_t k, uint64_t one, uint64_t n, uint64_t npi) {
   uint64_t t = one;
   while (k) {
-    if (k & 1) t = mont_prod64(t, a, n, npi);
+    if (k & 1) t = mont_mulmod(t, a, n);
     k >>= 1;
-    if (k)     a = mont_square64(a, n, npi);
+    if (k)     a = mont_sqrmod(a, n);
   }
   return t;
 }
-/* Returns -a^-1 mod 2^64.  From B. Arazi "On Primality Testing Using Purely
- * Divisionless Operations", Computer Journal (1994) 37 (3): 219-222, Proc 5.
- * Hensel lifting idea from Robert Gerbicz. */
-static INLINE uint64_t modular_inverse64(const uint64_t a)
-{
-  static const char mask[128] = {255,85,51,73,199,93,59,17,15,229,195,89,215,237,203,33,31,117,83,105,231,125,91,49,47,5,227,121,247,13,235,65,63,149,115,137,7,157,123,81,79,37,3,153,23,45,11,97,95,181,147,169,39,189,155,113,111,69,35,185,55,77,43,129,127,213,179,201,71,221,187,145,143,101,67,217,87,109,75,161,159,245,211,233,103,253,219,177,175,133,99,249,119,141,107,193,191,21,243,9,135,29,251,209,207,165,131,25,151,173,139,225,223,53,19,41,167,61,27,241,239,197,163,57,183,205,171,1};
-  uint64_t ret = mask[(a >> 1) & 127];
-  ret *= 2 + a * ret;
-  ret *= 2 + a * ret;
-  ret *= 2 + a * ret;
-  return ret;
-}
-static INLINE uint64_t compute_modn64(const uint64_t n)
-{
 
-  if (n <= (1ULL << 63)) {
-    uint64_t res = ((1ULL << 63) % n) << 1;
-    return res < n ? res : res-n;
-  } else
-    return -n;
+static INLINE uint64_t _u64div(uint64_t c, uint64_t n) {
+  asm("divq %4"
+      : "=a"(c), "=d"(n)
+      : "1"(c), "0"(0), "r"(n));
+  return n;
 }
-#ifdef FUNC_monty_mr64
-#define compute_a_times_2_64_mod_n(a, n, r)   mulmod(a, r, n)
-/* static INLINE int efficient_mr64(const uint64_t bases[], const int cnt, const uint64_t n) */
-static int monty_mr64(const uint64_t n, const UV* bases, int cnt)
-{
-  int i, j, t;
-  const uint64_t npi = modular_inverse64(n);
-  const uint64_t r = compute_modn64(n);
-  uint64_t u = n - 1;
-  const uint64_t nr = n - r;
 
-  t = 0;
-  while (!(u&1)) {  t++;  u >>= 1;  }
-  for (j = 0; j < cnt; j++) {
-    uint64_t d, a = bases[j];
-    uint64_t A = compute_a_times_2_64_mod_n(a, n, r);
-    if (a < 2)  croak("Base %"UVuf" is invalid", (UV)a);
-    if (a >= n) {
-      a %= n;
-      if (a == 0 || (a == n-1 && a&1)) return 0;
-    }
-    if (a == 1 || a == n-1) continue;
-    if (!A) continue;  /* PRIME in subtest */
-    d = mont_powmod64(A, u, r, n, npi);  /* compute a^u mod n */
-    if (d == r || d == nr) continue;  /* PRIME in subtest */
-    for (i=1; i<t; i++) {
-      d = mont_square64(d, n, npi);
-      if (d == r) return 0;
-      if (d == nr) break;  /* PRIME in subtest */
-    }
-    if (i == t) return 0;
-  }
-  return 1;
-}
-#endif
 #endif /* use_montmath */
 
 #endif
--- util.h
+++ util.h
@@ -170,12 +170,14 @@ static UV lcm_ui(UV x, UV y) {
 /* See:  http://mersenneforum.org/showpost.php?p=110896 */
 static int is_perfect_square(UV n)
 {
-  UV m;
-  m = n & 127;
+  UV m = n & 127;
   if ((m*0x8bc40d7d) & (m*0xa1e2f5d1) & 0x14020a)  return 0;
+  /* On x86-64, sqrt is fast enough to stop testing here */
+#if !defined(__x86_64__)
   /* This cuts out another 80%: */
   m = n % 63; if ((m*0x3d491df7) & (m*0xc824a9f9) & 0x10f14008) return 0;
   /* m = n % 25; if ((m*0x1929fc1b) & (m*0x4c9ea3b2) & 0x51001005) return 0; */
+#endif
   m = isqrt(n);
   return m*m == n;
 }
