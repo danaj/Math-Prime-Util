ce7fa39588fd86156d773cf5f3e18f56b7c613f7
Dana Jacobsen <dana@acm.org>
2017-10-12 17:26:32 -0700

    Updated mont_inverse

--- montmath.h
+++ montmath.h
@@ -21,22 +21,15 @@
 #define mont_powmod(a,k,n)        _powredc(a,k,mont1,n,npi)
 #define mont_recover(a,n)         mont_mulmod(a,1,n)
 
+/* See https://arxiv.org/pdf/1303.0328.pdf for lots of details on this.
+ * The 128-entry table solution is about 20% faster */
 static INLINE uint64_t mont_inverse(const uint64_t n) {
-#if 1
-  uint64_t ret = (((n + 2) & 4) << 1) + n;
-  ret *= 2 - n * ret;
-  ret *= 2 - n * ret;
-  ret *= 2 - n * ret;
-  ret *= 2 - n * ret;
+  uint64_t ret = (3*n) ^ 2;
+  ret *= (uint64_t)2 - n * ret;
+  ret *= (uint64_t)2 - n * ret;
+  ret *= (uint64_t)2 - n * ret;
+  ret *= (uint64_t)2 - n * ret;
   return (uint64_t)0 - ret;
-#else
-  static const char mask[128] = {255,85,51,73,199,93,59,17,15,229,195,89,215,237,203,33,31,117,83,105,231,125,91,49,47,5,227,121,247,13,235,65,63,149,115,137,7,157,123,81,79,37,3,153,23,45,11,97,95,181,147,169,39,189,155,113,111,69,35,185,55,77,43,129,127,213,179,201,71,221,187,145,143,101,67,217,87,109,75,161,159,245,211,233,103,253,219,177,175,133,99,249,119,141,107,193,191,21,243,9,135,29,251,209,207,165,131,25,151,173,139,225,223,53,19,41,167,61,27,241,239,197,163,57,183,205,171,1};
-  uint64_t ret = mask[(n >> 1) & 127];
-  ret *= 2 + n * ret;
-  ret *= 2 + n * ret;
-  ret *= 2 + n * ret;
-  return ret;
-#endif
 }
 
 static INLINE uint64_t _mulredc(uint64_t a, uint64_t b, uint64_t n, uint64_t npi) {
