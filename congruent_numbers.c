460118c9ef813ba3dd596d58abf58ba00531e212
Dana Jacobsen <dana@acm.org>
2025-06-09 20:13:45 +0700

    More work on congruent numbers (filtering and tests).

--- congruent_numbers.c
+++ congruent_numbers.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /*
  * A nice discussion of congruent numbers can be found in:
@@ -14,13 +15,12 @@
 #include "util.h"
 #include "factor.h"
 #include "rootmod.h"
-/* This is for randomly permuting the factors.  It will go away */
-#include "csprng.h"
+
+#define SWAP2(x,y) { UV t; t=x; x=y; y=t; }
 
 /******************************************************************************/
 
-/******************************************************************************/
-
+/* We only look at the non-square portion of n. */
 static void remove_square_part(factored_t *nf)   /* Turn n*c^2 into n */
 {
   if (nf->n > 3) {
@@ -43,21 +43,24 @@ static void remove_square_part(factored_t *nf)   /* Turn n*c^2 into n */
   /* factoredp_validate(nf); */
 }
 
-/******************************************************************************/
-
-static factored_t permute_odd_factors(factored_t nf, UV seed)
+/* Cycle through n! permutations of factors (if used). */
+static factored_t permute_odd_factors(const factored_t NF, UV k)
 {
-  void* rng = prng_new((seed>>16)>>16, seed & 0xFFFFFFFF, 0, 0);
-  uint16_t i, j;
+  factored_t nf;
+  int      permvec[MPU_MAX_DFACTORS];
+  bool     iseven = NF.f[0] == 2;
+  uint32_t noddfac = NF.nfactors - iseven;
+  uint16_t i;
 
-  /* for (i=0;i<nf.nfactors i++) printf(" %lu",nf.f[i]);  printf(" -> "); */
-  for (i = 1-(nf.n&1); i < nf.nfactors-1; i++) {
-    j = (uint16_t) prng_next(rng) % (nf.nfactors-i);
-    { UV t      = nf.f[i];  nf.f[i] = nf.f[i+j];  nf.f[i+j] = t; }
-    { uint8_t t = nf.e[i];  nf.e[i] = nf.e[i+j];  nf.e[i+j] = t; }
+  nf.n = NF.n;
+  nf.nfactors = NF.nfactors;
+  if (iseven) { nf.f[0] = 2;  nf.e[0] = NF.e[0]; }
+
+  num_to_perm(k, noddfac, permvec);
+  for (i = 0; i < noddfac; i++) {
+    nf.f[i + iseven] = NF.f[permvec[i] + iseven];
+    nf.e[i + iseven] = NF.e[permvec[i] + iseven];
   }
-  /* for (i=0;i<nf.nfactors i++) printf(" %lu",nf.f[i]);  printf("\n"); */
-  Safefree(rng);
   return nf;
 }
 
@@ -70,6 +73,9 @@ static factored_t permute_odd_factors(factored_t nf, UV seed)
  * Weak BSD holds for n <   71474.   (Wado 2005)
  * Weak BSD holds for n <  300000.   (Matsuno 2005]
  * Weak BSD holds for n < 1000000.   (Matsuno 2006 + Elkies 2002)
+ *
+ * The final answer.
+ * Most of the rest of this file is trying to avoid calling this if possible.
  */
 static bool _is_congruent_number_tunnell(UV n)
 {
@@ -107,21 +113,71 @@ static bool _is_congruent_number_tunnell(UV n)
 
 #define SWAP4(x,y) { UV t; t=x; x=y; y=t;  t=x##8; x##8=y##8; y##8=t; }
 #define KPQ kronecker_uu(p,q)
+#define KQP kronecker_uu(q,p)
+/* For 3 factors */
 #define KPR kronecker_uu(p,r)
 #define KQR kronecker_uu(q,r)
-/* Only used for a few cases: */
-#define KQP kronecker_uu(q,p)
 #define KRP kronecker_uu(r,p)
 #define KRQ kronecker_uu(r,q)
+/* For 4 factors */
+#define KPS kronecker_uu(p,s)
+#define KQS kronecker_uu(q,s)
+#define KRS kronecker_uu(r,s)
+#define KSP kronecker_uu(s,p)
+#define KSQ kronecker_uu(s,q)
+#define KSR kronecker_uu(s,r)
+
 #define LAGRANGE_COND1 (KPQ==KPR || KQR==KQP || KRP==KRQ)
 #define LAGRANGE_COND2 ((KPQ==1 && KPR==1) || (KQR==1 && KQP==1) || (KRP==1 && KRQ==1))
 #define LAGRANGE_COND3 ((KPQ==-1 && KPR==-1) || (KQR==-1 && KQP==-1) || (KRP==-1 && KRQ==-1))
 
+static bool _can_order_kronecker2(UV p, UV q) {
+  return KPQ==-1 || KQP == -1;
+}
+static bool _can_order_kronecker3(UV p, UV q, UV r) {
+  return (KPR==-1 && KQR==-1 && _can_order_kronecker2(p,q)) ||
+         (KPQ==-1 && KRQ==-1 && _can_order_kronecker2(p,r)) ||
+         (KQP==-1 && KRP==-1 && _can_order_kronecker2(q,r));
+}
+static bool _can_order_kronecker4(UV p, UV q, UV r, UV s) {
+  return (KPS==-1 && KQS==-1 && KRS==-1 && _can_order_kronecker3(p,q,r)) ||
+         (KPR==-1 && KQR==-1 && KSR==-1 && _can_order_kronecker3(p,q,s)) ||
+         (KPQ==-1 && KRQ==-1 && KSQ==-1 && _can_order_kronecker3(p,r,s)) ||
+         (KQP==-1 && KRP==-1 && KSP==-1 && _can_order_kronecker3(q,r,s));
+}
+
+static bool _can_orderk_r(uint32_t nfac, UV fac[]) {
+  uint32_t i,j;
+  if (nfac <= 1) return TRUE;
+  if (nfac == 2) return _can_order_kronecker2(fac[0],fac[1]);
+  for (i = 0; i < nfac; i++) {
+    SWAP2(fac[i], fac[nfac-1]);  /* Test with this factor at the end */
+    for (j = 0; j < nfac-1; j++)
+      if (kronecker_uu(fac[j],fac[nfac-1]) != -1)
+        break;
+    if (j == nfac-1 && _can_orderk_r(nfac-1, fac))
+      break;
+   }
+   return i < nfac;
+}
+/* Can we order s.t. (p|q) == (p|r) == (q|r) == -1 */
+static bool _can_orderk(uint32_t nfac, const UV fac[]) {
+  UV F[MPU_MAX_DFACTORS];
+  if (nfac > MPU_MAX_DFACTORS) return FALSE;
+  memcpy(F, fac, nfac*sizeof(UV));  /* We can safely permute the ordering */
+  return _can_orderk_r(nfac, F);
+}
+
 /* Returns -1 if not known, 0 or 1 indicate definite results. */
-int _is_congruent_number_filter1(const factored_t nf) {
-  const UV *fac      = nf.f;
+static int _is_congruent_number_filter1(const factored_t nf) {
   const UV n         = nf.n;
-  const int nfactors = nf.nfactors;
+  const bool isodd   = n & 1;
+  const bool iseven  = !isodd;
+
+  UV p,  q,  r,  s;   /* Four odd factors in mod 8 order */
+  UV p8, q8, r8, s8;  /* values mod 8 */
+  UV fac[MPU_MAX_DFACTORS];  /* The odd factors, in mod 8 order */
+  uint32_t nfac = 0;
 
   MPUassert(n >= 13, "n too small in icn_filter");
 
@@ -131,19 +187,32 @@ int _is_congruent_number_filter1(const factored_t nf) {
    */
   if (n % 8 == 5 || n % 8 == 6 || n % 8 == 7)  return 1;
 
+  { /* Sort odd factors into fac array by mod 8 */
+    uint32_t i;
+    for (i=0; i<nf.nfactors; i++) if (nf.f[i] % 8 == 1) fac[nfac++] = nf.f[i];
+    for (i=0; i<nf.nfactors; i++) if (nf.f[i] % 8 == 3) fac[nfac++] = nf.f[i];
+    for (i=0; i<nf.nfactors; i++) if (nf.f[i] % 8 == 5) fac[nfac++] = nf.f[i];
+    for (i=0; i<nf.nfactors; i++) if (nf.f[i] % 8 == 7) fac[nfac++] = nf.f[i];
+  }
+  p = fac[0];
+  q = (nfac > 1) ? fac[1] : 0;
+  r = (nfac > 2) ? fac[2] : 0;
+  s = (nfac > 3) ? fac[3] : 0;
+  p8 = p % 8;  q8 = q % 8;  r8 = r % 8;  s8 = s % 8;
+
   /* Evink 2021    https://arxiv.org/pdf/2105.01450.pdf
    * Feng 1996     http://matwbn.icm.edu.pl/ksiazki/aa/aa75/aa7513.pdf
    * Monsky 1990   https://gdz.sub.uni-goettingen.de/id/PPN266833020_0204
    * Lagrange 1974 https://www.numdam.org/item/SDPP_1974-1975__16_1_A11_0.pdf
    */
 
-  if (nfactors == 1) {                   /* n = p */
+  if (isodd && nfac == 1) {          /* n = p */
 
-    UV r, p = n, p8 = p % 8;
+    UV root;
     if (p8 == 3) return 0;  /* Genocchi 1855 */
 
     /* https://arxiv.org/pdf/2105.01450.pdf, Prop 2.1.2 */
-    if (sqrtmodp(&r, 2, p) && kronecker_uu(1+r, p) == -1)
+    if (sqrtmodp(&root, 2, p) && kronecker_uu(1+root, p) == -1)
       return 0;
 #if 0
     { /* Evink 2021 shows these are equivalent to the sqrt test above */
@@ -159,17 +228,14 @@ int _is_congruent_number_filter1(const factored_t nf) {
     }
 #endif
 
-  } else if (!(n&1) && nfactors == 2) {  /* n = 2p */
+  } else if (iseven && nfac == 1) {  /* n = 2p */
 
-    UV p = n >> 1, p8 = p % 8;
     if (p8 == 3 || p8 == 7)     return 1;  /* we already returned 1 earlier */
     if (p8 == 5)                return 0;  /* Genocchi 1855 */
     if (p % 16 == 9)            return 0;  /* Bastien 1915 */
 
-  } else if ( (n&1) && nfactors == 2) {  /* n = pq */
+  } else if (isodd && nfac == 2) {  /* n = pq */
 
-    UV p = fac[0], q = fac[1],  p8 = p % 8, q8 = q % 8;
-    if (p8 > q8) SWAP4(p,q);
     if (p8 == 3 && q8 == 3) return 0;  /* Genocchi 1855 */
 #if 0  /* Monsky, all produce n mod 8 = 5 or 7: we already returned 1 */
     if (p8 == 3 && q8 == 7) return 1;
@@ -181,10 +247,8 @@ int _is_congruent_number_filter1(const factored_t nf) {
     if (p8 == 1 && q8 == 3 && KPQ == -1) return 0;
     if (p8 == 5 && q8 == 7 && KPQ == -1) return 0;
 
-  } else if (!(n&1) && nfactors == 3) {  /* n = 2pq */
+  } else if (iseven && nfac == 2) {  /* n = 2pq */
 
-    UV p = fac[1], q = fac[2],  p8 = p % 8, q8 = q % 8;
-    if (p8 > q8) SWAP4(p,q);
     if (p8 == 5 && q8 == 5) return 0; /* Genocchi 1855 */
 #if 0  /* Monsky, all produce n mod 8 = 6: we already returned 1 */
     if (p8 == 3 && q8 == 5) return 1;
@@ -199,12 +263,8 @@ int _is_congruent_number_filter1(const factored_t nf) {
     if (p8 == 7 && q8 == 7 && KPQ == 1 && q % 16 == 7) return 0;
     if (p8 == 1 && q8 == 1 && KPQ == -1 && (p*q) % 16 == 9) return 0;
 
-  } else if ( (n&1) && nfactors == 3) {  /* n = pqr */
+  } else if (isodd && nfac == 3) {  /* n = pqr */
 
-    UV p=fac[0], q=fac[1], r=fac[2], p8=fac[0]%8, q8=fac[1]%8, r8=fac[2]%8;
-    if (q8 < p8) SWAP4(p,q);
-    if (r8 < q8) SWAP4(q,r);
-    if (q8 < p8) SWAP4(p,q);
 #if 0 /* Serf 1991, all produce n mod 8 = 5 or 7: we already returned 1 */
     if (p8 == 3 && q8 == 3 && r8 == 5) return 1;
     if (p8 == 3 && q8 == 3 && r8 == 7) return 1;
@@ -224,12 +284,8 @@ int _is_congruent_number_filter1(const factored_t nf) {
         return 0;
     }
 
-  } else if (!(n&1) && nfactors == 4) {  /* n = 2pqr */
+  } else if (iseven && nfac == 3) {  /* n = 2pqr */
 
-    UV p=fac[1], q=fac[2], r=fac[3], p8=fac[1]%8, q8=fac[2]%8, r8=fac[3]%8;
-    if (q8 < p8) SWAP4(p,q);
-    if (r8 < q8) SWAP4(q,r);
-    if (q8 < p8) SWAP4(p,q);
 #if 0 /* Serf 1991, all produce n mod 8 = 6: we already returned 1 */
     if (p8 == 3 && q8 == 3 && r8 == 7) return 1;
     if (p8 == 3 && q8 == 5 && r8 == 5) return 1;
@@ -243,29 +299,93 @@ int _is_congruent_number_filter1(const factored_t nf) {
     if (p8 == 1 && q8 == 1 && r8 == 1 && LAGRANGE_COND3 && (p*q*r) % 16 == 9) return 0;
     if (p8 == 5 && q8 == 7 && r8 == 7 && KQP == KQR && KQP == -KRP) return 0;
     if (p8 == 1 && q8 == 1 && r8 == 5 && LAGRANGE_COND3) return 0;
-    /* 1 3 7 is not checked.
+    /* Lagrange's 1 3 7 seems to be incorrect.
      * 13706 = 2*7*11*89 = 2*89*7*11, so p = 1, q = -1, r = 3 mod 8.
      *       cond3 (q|r)= (q|p) = -1.
-     * but 13706 is a congruent number.  So this seems to be incorrect.
+     * but 13706 is a congruent number.
+     * Cheng/Guo 2018 show this case with a third congruency.
      */
     if (p8 == 3 && q8 == 3 && r8 == 5 && LAGRANGE_COND1) return 0;
     if (p8 == 5 && q8 == 5 && r8 == 5 && LAGRANGE_COND2) return 0;
+    /* Cheng/Guo 2018, Theorem 1.3.6 */
+    if (p8 == 1 && q8 == 3 && r8 == 7 && KPQ == -1 && KPR == -1 && KQR == -1) return 0;
 
-  } else if ( (n&1) && nfactors == 4) {  /* n = pqrs */
+  } else if (isodd && nfac == 4) {  /* n = pqrs */
 
-    UV p=fac[0], q=fac[1], r=fac[2], s=fac[3];
-    UV p8=fac[0]%8, q8=fac[1]%8, r8=fac[2]%8, s8=fac[3]%8;
-    if (r8 < p8) SWAP4(p,r);
-    if (s8 < q8) SWAP4(q,s);
-    if (q8 < p8) SWAP4(p,q);
-    if (s8 < r8) SWAP4(r,s);
-    if (r8 < q8) SWAP4(q,r);
     /* Serf 1991 */
     if (p8 == 5 && q8 == 5 && r8 == 7 && s8 == 7 &&
-        ( (KPR ==  1 && KQR == -1 && kronecker_uu(p,s) == -1) ||
-          (KPR == -1 && kronecker_uu(p,s) ==  1 && kronecker_uu(q,s) == -1) ||
-          (KPR == -1 && kronecker_uu(p,s) == -1 && KQR == -kronecker_uu(q,s))))
+        ( (KPR ==  1 && KQR == -1 && KPS == -1) ||
+          (KPR == -1 && KPS ==  1 && KQS == -1) ||
+          (KPR == -1 && KPS == -1 && KQR == -KQS)))
       return 0;
+    /* Cheng/Guo 2018, Theorem 1.3.1 */
+    if (p8 == 3 && q8 == 3 && r8 == 5 && s8 == 7 &&
+        (KPQ == -1 || KQP == -1) &&
+        KPR == -1 && KQR == -1 && KPS == -1 && KQS == -1 && KRS == -1)
+      return 0;
+    /* Cheng/Guo 2018, Theorem 1.2.5 */
+    if (p8 == 1 && q8 == 3 && r8 == 3 && s8 == 3 &&
+        KPQ == -1 && KPR == -1 && KPS == -1 &&
+        _can_order_kronecker3(q,r,s))
+      return 0;
+    /* Iskra 1996 (also Cheng/Guo 2018, Theorem 1.1.2) */
+    if (p8 == 3 && q8 == 3 && r8 == 3 && s8 == 3 &&
+        _can_order_kronecker4(p,q,r,s))
+      return 0;
+    /* Das 2020 4 factors */
+    if (p8 == 1 && q8 == 3 && r8 == 5 && s8 == 7 &&
+        (KQS == -1 || KSQ == -1) &&
+         KPR == 1 && KRP == 1 && KPS == 1 && KRQ == 1 && KPQ == -1 && KRS == -1)
+      return 0;
+    /* Das 2020 4 factors */
+    if (p8 == 1 && q8 == 1 && r8 == 3 && s8 == 3 &&
+        (KRS == -1 || KSR == -1) && KPQ == 1 && KQP == 1 &&
+        ( (KPR == -1 && KQS == -1 && KPS == 1 && KQR == 1) ||
+          (KQR == -1 && KPS == -1 && KQS == 1 && KPR == 1) ))
+      return 0;
+
+  } else if (iseven && nfac == 4) {  /* n = 2pqrs */
+
+    /* Serf 1991 */
+    if (p8 == 1 && q8 == 1 && r8 == 3 && s8 == 3 &&
+        ( (KPQ ==  1 && KPR == -KPS && KQR == -KQS) ||
+          (KPQ == -1 && KPR ==  KPS && KQR == -KQS) ||
+          (KPQ == -1 && KPR == -KPS)))
+      return 0;
+    /* Cheng/Guo 2018, Theorem 1.3.2 */
+    if (p8 == 3 && q8 == 5 && r8 == 5 && s8 == 7 &&
+        (KQR == -1 || KRQ == -1) &&
+        KPQ == -1 && KPR == -1 && KPS == -1 && KQS == -1 && KRS == -1)
+      return 0;
+    /* Cheng/Guo 2018, Theorem 1.3.8 */
+    if (p8 == 1 && q8 == 3 && r8 == 3 && s8 == 5 &&
+        (KQR == -1 || KRQ == -1) &&
+        KPQ == -1 && KPR == -1 && KPS == -1 && KQS == -1 && KRS == -1)
+      return 0;
+    /* Cheng/Guo 2018, Theorem 1.2.3 */
+    if (p8 == 3 && q8 == 3 && r8 == 3 && s8 == 7 &&
+        KPS == -1 && KQS == -1 && KRS == -1 &&
+        _can_order_kronecker3(p,q,r))
+      return 0;
+    /* Cheng/Guo 2018, Theorem 1.2.6 */
+    if (p8 == 1 && q8 == 5 && r8 == 5 && s8 == 5 &&
+        KPQ == -1 && KPR == -1 && KPS == -1 &&
+        _can_order_kronecker3(q,r,s))
+      return 0;
+    /* Cheng/Guo 2018, Theorem 1.1.1 (even analog of Iskra 1996) */
+    if (p8 == 3 && q8 == 3 && r8 == 3 && s8 == 3 &&
+        _can_order_kronecker4(p,q,r,s))
+      return 0;
+    /* Cheng/Guo 2018, Theorem 1.1.3 */
+    if (p8 == 5 && q8 == 5 && r8 == 5 && s8 == 5 &&
+        _can_order_kronecker4(p,q,r,s))
+      return 0;
+    /* Cheng/Guo 2018, Theorem 1.2.1 */
+    if (p8 == 3 && q8 == 3 && r8 == 5 && s8 == 5 &&
+        (KPQ == -1 || KQP == -1) && (KRS == -1 || KSR == -1) &&
+        KPR == -1 && KQR == -1 && KPS == -1 && KQS == -1)
+      return 0;
+
   }
 
   return -1;
@@ -273,30 +393,52 @@ int _is_congruent_number_filter1(const factored_t nf) {
 
 /******************************************************************************/
 
+/* This has more complicated filters that take arbitrary numbers of factors,
+ * and have to handle permutations.
+ */
+
 /* Returns -1 if not known, 0 or 1 indicate definite results. */
-int _is_congruent_number_filter2(const factored_t nf) {
+static int _is_congruent_number_filter2(const factored_t nf) {
+  const bool     iseven  = nf.f[0] == 2;
+  const uint32_t noddfac = nf.nfactors - iseven;
+  const UV      *oddfac  = nf.f + iseven;
+  uint16_t i;
+  bool allmod3;
+
+  for (i = 0; i < noddfac; i++)
+    if (oddfac[i] % 8 != 3)
+      break;
+  allmod3 = !(i < noddfac);
+
+  /* Iskra 1996 (odd) ; Cheng/Guo 2019 Theorem 1.1.1 (even) */
+  if (allmod3 && _can_orderk(noddfac, oddfac)) return 0;
+
+  return -1;
+}
+
+/******************************************************************************/
+
+/* More complicated filters that are factor-permutation dependent, but don't
+ * yet have code to understand that.  Ideally that would get done, they would
+ * be moved to _filter2, and this function would go away.
+ *
+ * Currently all inputs with fewer than 4 odd factors are handled earlier.
+ *
+ * For now we either call it once optimistically, or we call it multiple times
+ * with permuted factors.
+ */
+
+/* Returns -1 if not known, 0 or 1 indicate definite results. */
+static int _is_congruent_number_filter3(const factored_t nf) {
   const UV *fac      = nf.f;
   const UV n         = nf.n;
   const int nfactors = nf.nfactors;
   int i, j;
 
-  /* All of these tests seem complicated and tedious.  The goal is to avoid
-   * the Tunnell counting loop at the end if at all possible.  While that
-   * loop is fast and simple compared to the method of descent, it is still
-   * very, very time consuming for any reasonable size input.  So we make
-   * an effort to identify known families.
-   *
-   * These filters all require looking at multiple orderings of the factors.
-   * The conditions are true if *any* ordering meets the conditions.
-   *
-   * TODO: currently none of these look at different orderings.  So they will
-   *       answer "I don't know" instead of flagging some numbers as non-cong.
-   */
-
   /* Reinholz 2013 https://central.bac-lac.gc.ca/.item?id=TC-BVAU-44941&op=pdf
-   * Cheng 2018   http://maths.nju.edu.cn/~guoxj/articles/IJNT2019.pdf
-   * Cheng 2019   https://www.sciencedirect.com/science/article/pii/S0022314X18302774
-   * Das 2020     https://math.colgate.edu/~integers/u55/u55.pdf
+   * Cheng 2018    http://maths.nju.edu.cn/~guoxj/articles/IJNT2019.pdf
+   * Cheng 2019    https://www.sciencedirect.com/science/article/pii/S0022314X18302774
+   * Das 2020      https://math.colgate.edu/~integers/u55/u55.pdf
    */
 
   {
@@ -307,14 +449,6 @@ int _is_congruent_number_filter2(const factored_t nf) {
     for (i = 1;  allmod3 && i <= noddfactors;  i++)
       if ((oddfac[i-1] % 8) != 3)
         allmod3 = 0;
-
-    if (allmod3) { /* Iskra 1996 (odd) ; Cheng,Guo 2019 (even) */
-      int iskra = 1;
-      for (i = 2; iskra && i <= noddfactors; i++)
-        for (j = 1; iskra && j < i; j++)
-          iskra &= kronecker_uu(oddfac[j-1],oddfac[i-1]) == -1;
-      if (iskra) return 0;
-    }
 
     /* Reinholz, Spearman, Yang 2013 */
     if (allmod3 && (n&1)) {
@@ -489,6 +623,23 @@ int is_congruent_number_filter(UV n) {
   res = _is_congruent_number_filter1(nf);
   if (res != -1) return res;
   res = _is_congruent_number_filter2(nf);
+  if (res != -1) return res;
+  res = _is_congruent_number_filter3(nf);
+  if (res != -1) return res;
+
+#if 0
+  if (1 && res == -1) {
+    uint32_t noddfac = nf.nfactors - (nf.f[0] == 2);
+    if (noddfac > 3) {
+      UV i, nperms = factorial(noddfac);
+      for (i = 1; res == -1 && i < nperms; i++) {
+        factored_t trynf = permute_odd_factors(nf, i);
+        res = _is_congruent_number_filter3(trynf);
+      }
+    }
+  }
+  /* if (res != -1) printf("%lu\n", nf.n); */
+#endif
   return res;
 }
 bool is_congruent_number_tunnell(UV n) {
@@ -509,17 +660,24 @@ bool is_congruent_number(UV n)
   remove_square_part(&nf);
   if (nf.n < 13) return (nf.n >= 5 && nf.n <= 7);
 
+  /* Relatively simple filters.  Order doesn't matter. */
   res = _is_congruent_number_filter1(nf);
   if (res != -1) return res;
+  /* More complicated filters.  Permutation is handled. */
   res = _is_congruent_number_filter2(nf);
   if (res != -1) return res;
+  /* More complicated filters.  We haven't implemented permutations. */
+  res = _is_congruent_number_filter3(nf);
+  if (res != -1) return res;
 
-  /* Hacky fun time, randomly permute factors a few times */
-  if (0) {
-    int i;
-    for (i = 1; res == -1 && i <= 10; i++) {
-      nf = permute_odd_factors(nf, i);
-      res = _is_congruent_number_filter2(nf);
+  if (0) {  /* Try filter3 with all odd factor permutations */
+    uint32_t noddfac = nf.nfactors - (nf.f[0] == 2);
+    if (noddfac > 3) {
+      UV i, nperms = factorial(noddfac);
+      for (i = 1; res == -1 && i < nperms; i++) {
+        factored_t trynf = permute_odd_factors(nf, i);
+        res = _is_congruent_number_filter3(trynf);
+      }
     }
     if (res != -1) return res;
   }
